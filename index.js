b74cab3c4710f82ded94447e9194958f0e8c38cd
aaakash06 <akashbagale182@gmail.com>
2024-05-06 18:01:41 +0545

    port started

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import WebSocket from "ws";
+const http = require("http");
 const WSS = "wss://hamro-chess-1.onrender.com";
 const socket = new WebSocket(WSS);
 socket.onopen = () => {
   console.log("connected... about to close connection");
   socket.close();
 };
+
 const timeInterval = 60 * 30 * 1000;
 setInterval(() => {
   const socket = new WebSocket(WSS);
@@ -13,3 +15,8 @@ setInterval(() => {
     socket.close();
   };
 }, timeInterval);
+
+const server = http.createServer();
+server.listen(3000, () => {
+  console.log("server is listening");
+});
